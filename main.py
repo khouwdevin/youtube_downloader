430df4c5cea409b60502049f3c51061f038fdc28
khouwdevin <khouwdevin@gmail.com>
2023-04-14 15:36:28 +0700

    init commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+from src.mainpage import MainPage
+
+page = [MainPage]
+
+class App(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        self.geometry("600x300")
+        self.title("Youtube Downloader")
+        self.iconphoto(False, tk.PhotoImage(file = r"./public/icon.png"))
+         
+        container = tk.Frame(self) 
+        container.pack(side = "top", fill = "both", expand = True)
+  
+        container.grid_rowconfigure(0, weight = 1)
+        container.grid_columnconfigure(0, weight = 1)
+  
+        # initializing frames to an empty array
+        self.frames = {} 
+  
+        # iterating through a tuple consisting
+        # of the different page layouts
+        for F in page:
+            frame = F(container, self)
+  
+            # initializing frame of that object from
+            # startpage, page1, page2 respectively with
+            # for loop
+            self.frames[F] = frame
+  
+            frame.grid(row = 0, column = 0, sticky ="nsew")
+  
+        self.show_frame(MainPage)
+  
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
